f8991d6f944e6ac741e629b49260fc106238c8b7
C J Silverio <ceej@npmjs.com>
2015-04-13 10:17:59 -0700

    Don't create mailchimp interfaces every time we need to send email.
    Instead, create one once & hold onto it.
    
    Also, fix all things jshint complained about.

--- models/user.js
+++ models/user.js
@@ -3,13 +3,15 @@ var cache = require('../lib/cache');
 var decorate = require(__dirname + '/../presenters/user');
 var fmt = require('util').format;
 var mailchimp = require('mailchimp-api');
-var Promise = require('bluebird');
+var P = require('bluebird');
 var request = require('request');
 var userValidate = require('npm-user-validate');
 
+var chimp;
+
 var User = module.exports = function(opts) {
   _.extend(this, {
-    host: process.env.USER_API || "https://user-api-example.com",
+    host: process.env.USER_API || 'https://user-api-example.com',
     bearer: false
   }, opts);
 
@@ -29,10 +31,10 @@ User.new = function(request) {
 };
 
 User.prototype.confirmEmail = function (user, callback) {
-  var url = fmt("%s/user/%s/verify", this.host, user.name);
+  var url = fmt('%s/user/%s/verify', this.host, user.name);
   this.log(url);
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     var opts = {
       url: url,
       json: true,
@@ -54,9 +56,9 @@ User.prototype.confirmEmail = function (user, callback) {
 };
 
 User.prototype.login = function(loginInfo, callback) {
-  var url = fmt("%s/user/%s/login", this.host, loginInfo.name);
+  var url = fmt('%s/user/%s/login', this.host, loginInfo.name);
 
-  return new Promise(function (resolve, reject) {
+  return new P(function (resolve, reject) {
     request.post({
       url: url,
       json: true,
@@ -86,7 +88,7 @@ User.prototype.login = function(loginInfo, callback) {
 
 User.prototype.generateUserACLOptions = function generateUserACLOptions(name) {
   return {
-    url: fmt("%s/user/%s", this.host, name),
+    url: fmt('%s/user/%s', this.host, name),
     json: true,
   };
 };
@@ -104,7 +106,7 @@ User.prototype.get = function(name, options, callback) {
     options = {};
   }
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
       cache.get(_this.generateUserACLOptions(name), function(err, body){
         if (err) { return reject(err); }
         return resolve(body);
@@ -128,9 +130,9 @@ User.prototype.get = function(name, options, callback) {
 
 User.prototype.getPackages = function(name, callback) {
   var _this = this;
-  var url = fmt("%s/user/%s/package", this.host, name);
+  var url = fmt('%s/user/%s/package', this.host, name);
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     var opts = {
       url: url,
       qs: {
@@ -140,7 +142,7 @@ User.prototype.getPackages = function(name, callback) {
       json: true
     };
 
-    if (_this.bearer) opts.headers = {bearer: _this.bearer};
+    if (_this.bearer) { opts.headers = {bearer: _this.bearer}; }
 
     request.get(opts, function(err, resp, body){
 
@@ -157,15 +159,15 @@ User.prototype.getPackages = function(name, callback) {
 
 User.prototype.getStars = function(name, callback) {
   var _this = this;
-  var url = fmt("%s/user/%s/stars?format=detailed", this.host, name);
+  var url = fmt('%s/user/%s/stars?format=detailed', this.host, name);
 
-  return new Promise(function(resolve, reject) {
+  return new P(function(resolve, reject) {
     var opts = {
       url: url,
       json: true
     };
 
-    if (_this.bearer) opts.headers = {bearer: _this.bearer};
+    if (_this.bearer) { opts.headers = {bearer: _this.bearer}; }
 
     request.get(opts, function(err, resp, body){
 
@@ -186,10 +188,9 @@ User.prototype.log = function(msg) {
 };
 
 User.prototype.login = function(loginInfo, callback) {
-  var url = fmt("%s/user/%s/login", this.host, loginInfo.name);
-  this.log(url);
+  var url = fmt('%s/user/%s/login', this.host, loginInfo.name);
 
-  return new Promise(function (resolve, reject) {
+  return new P(function (resolve, reject) {
 
     request.post({
       url: url,
@@ -221,15 +222,15 @@ User.prototype.login = function(loginInfo, callback) {
 User.prototype.lookupEmail = function(email, callback) {
   var _this = this;
 
-  return new Promise(function (resolve, reject) {
-    if(userValidate.email(email)) {
+  return new P(function (resolve, reject) {
+    if (userValidate.email(email)) {
       var err = new Error('email is invalid');
       err.statusCode = 400;
       _this.log(err);
       return reject(err);
     }
 
-    var url = _this.host + "/user/" + email;
+    var url = _this.host + '/user/' + email;
     _this.log(url);
 
     request.get({url: url, json: true}, function (err, resp, body) {
@@ -246,9 +247,9 @@ User.prototype.lookupEmail = function(email, callback) {
 };
 
 User.prototype.save = function (user, callback) {
-  var url = this.host + "/user/" + user.name;
+  var url = this.host + '/user/' + user.name;
 
-  return new Promise(function (resolve, reject) {
+  return new P(function (resolve, reject) {
     var opts = {
       url: url,
       json: true,
@@ -270,9 +271,9 @@ User.prototype.save = function (user, callback) {
 User.prototype.signup = function (user, callback) {
   var _this = this;
 
-  if (user.npmweekly === "on") {
+  if (user.npmweekly === 'on') {
     var mc = this.getMailchimp();
-    mc.lists.subscribe({id: 'e17fe5d778', email:{email:user.email}}, function(data) {
+    mc.lists.subscribe({id: 'e17fe5d778', email:{email:user.email}}, function() {
       // do nothing on success
     }, function(error) {
       _this.logger.error('Could not register user for npm Weekly: ' + user.email);
@@ -282,9 +283,9 @@ User.prototype.signup = function (user, callback) {
     });
   }
 
-  var url = this.host + "/user";
+  var url = this.host + '/user';
 
-  return new Promise(function (resolve, reject) {
+  return new P(function (resolve, reject) {
     var opts = {
       url: url,
       body: user,
@@ -305,7 +306,10 @@ User.prototype.signup = function (user, callback) {
 };
 
 User.prototype.getMailchimp = function getMailchimp () {
-  return new mailchimp.Mailchimp(process.env.MAILCHIMP_KEY);
+  if (!chimp) {
+    chimp = new mailchimp.Mailchimp(process.env.MAILCHIMP_KEY);
+  }
+  return chimp;
 };
 
 User.prototype.verifyPassword = function (name, password, callback) {
